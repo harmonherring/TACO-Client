a227846a3b7ad2cd9fb3407c2c309987035b35e1
Grace Lombardi <grace@csh.rit.edu>
2019-02-23 17:38:57 -0500

    I created a python program that can send packets to a certain IP address

---
+++ packets.py
@@ -0,0 +1,18 @@
+"""
+Grace Lombardi
+I made a program that sends 100 packets to a chosen IP and port
+"""
+
+import socket
+import random
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1362)
+ip = input('Target IP: ')
+sent = 0
+i = 100
+while i >= 0:
+    sock.sendto(bytes,(ip,53))
+    print("Sent %s amount of packets to %s at port %s." % (sent, ip, 53))
+    sent = sent + 1
+    i = i-1
